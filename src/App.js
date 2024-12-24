9819790a14659647422019474fe0d37ebe8b0f99
Adeel940 <kbwadeel@gmail.com>
2024-12-24 10:56:06 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='bg-slate-500 text-red-800 text-start'>hello </h2>
+    </div>
+  );
+}
+
+export default App;
